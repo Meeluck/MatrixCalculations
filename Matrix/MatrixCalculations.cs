e3e80cb15a49cf1cde4e95af9a84773a10ecf22f
Meeluck <50030554+Meeluck@users.noreply.github.com>
2020-06-18 11:38:40 +0300

    Add project files.

---
+++ Matrix/MatrixCalculations.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+	public class MatrixCalculations 
+	{
+        #region Определитель
+        //получение миноров для расчета определителя матрицы
+        static void GetMinor(int[,] matr, ref int[,] p, int i, int j, int s)
+        {
+            int ki;
+            int kj;
+            int di = 0;
+            int dj = 0;
+
+            for (ki = 0; ki < s - 1; ki++)
+            {
+                // проверка индекса строки
+                if (ki == i)
+                    di = 1;
+                dj = 0;
+                for (kj = 0; kj < s - 1; kj++)
+                {
+                    // проверка индекса столбца
+                    if (kj == j)
+                        dj = 1;
+                    p[ki, kj] = matr[ki + di, kj + dj];
+                }
+            }
+        }
+
+        public static int Determinant(int[,] matr, int s)
+        {
+
+            int i, d, k, n;
+            int[,] p = new int[s, s]; //матрица для миноров 
+            d = 0; //детерминанте
+            k = 1; //(-1) в степени i
+            n = s - 1; //размер минора
+
+            if (s == 1)
+            {
+                d = matr[0, 0];
+                return d;
+            }
+
+            if (s == 2)
+            {
+                d = (matr[0, 0] * matr[1, 1]) - matr[1, 0] * matr[0, 1];
+                return d;
+            }
+
+            if (s > 2)
+            {
+                for (i = 0; i < s; i++)
+                {
+                    GetMinor(matr, ref p, i, 0, s);
+                    d = d + k * matr[i, 0] * Determinant(p, n);
+                    k = -k;
+                }
+            }
+
+            return d;
+        }
+        #endregion
+
+        #region Ранг матрицы
+        static void Swap(int[,] mat, int row1, int row2, int col)
+        {
+            for (int i = 0; i < col; i++)
+            {
+                int temp = mat[row1, i];
+                mat[row1, i] = mat[row2, i];
+                mat[row2, i] = temp;
+            }
+        }
+        public static void Display(int[,] mat, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+
+                for (int j = 0; j < col; j++)
+                    Console.Write(" " + mat[i, j]);
+                Console.Write("\n");
+            }
+        }
+
+        // function for finding rank of matrix  
+        public static int RankOfMatrix(int[,] mat, int R, int C)
+        {
+            int rank = Math.Min(R, C);
+
+            for (int row = 0; row < rank; row++)
+            {
+                //Приводим матрицу к ступенчатому виду
+                if (mat[row, row] != 0)
+                {
+                    for (int col = 0; col < R; col++)
+                    {
+                        if (col != row)
+                        {
+                            double mult = (double)mat[col, row] / mat[row, row];
+
+                            for (int i = 0; i < rank; i++)
+                                mat[col, i] -= (int)(mult * mat[row, i]);
+                        }
+                    }
+                }
+
+                /*
+                 * Если диагональный элемент равен 0 возникает 2 случая
+                 *  1)  Если под этим элементом есть строка с ненулевым элементом
+                 *      меняем местами тещую строку с той строкой, где найден 
+                 *      ненулевой элемент
+                 *  2)  Если все элементы текущем столбце ниже диагонального 
+                 *      элемента равны 0, тогда меняем местами данные столбец 
+                 *      с крайним и уменьшаем количество столбцов на 1
+                 */
+
+                else
+                {
+                    bool reduce = true;
+
+                    // Ищем ненулевой элемент в текущем столбце
+                    for (int i = row + 1; i < R; i++)
+                    {
+                        //меняем строку с ненулевым элементов на текущую строку
+                        if (mat[i, row] != 0)
+                        {
+                            Swap(mat, row, i, rank);
+                            reduce = false;
+                            break;
+                        }
+                    }
+
+                    /*
+                     * Если мы не нашли ни одной строки с ненулевым элементом
+                     * в текущем столбце, то все значения в этом столбце равны 0.
+                     */
+                    if (reduce)
+                    {
+                        //Уменьшаяем ранг матрицы
+                        rank--;
+
+                        // Копируем последний столбце
+                        for (int i = 0; i < R; i++)
+                            mat[i, row] = mat[i, rank];
+                    }
+                    row--;
+                }
+            }
+
+            return rank;
+        }
+        #endregion
+
+        #region Собсвтенные значения матрицы
+        public static void Eigenvalues()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+	class Program
+	{
+        static void TestRank()
+        {
+            int[,] matrix1 =
+            {
+                {1,0,2 },
+                {0,2,0 },
+                {2,0,3 }
+            };
+            MatrixCalculations.Display(matrix1, 3, 3);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix1, 3, 3));
+
+
+            int[,] matrix2 =
+            {
+                {3,-3,-5,8 },
+                {-3,2,4,-6 },
+                {2,-5,-7,5 },
+                {-4,3,5,-6 }
+            };
+            MatrixCalculations.Display(matrix2, 4, 4);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix2, 4, 4));
+
+            int[,] matrix3 =
+            {
+                {2,4,6},
+                {1,2,3},
+                {3,6,9}
+            };
+            MatrixCalculations.Display(matrix3, 3, 3);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix3, 3, 3));
+
+            int[,] matrix4 =
+                {
+                    {10, 20, 10},
+                    {-20, -30, 10},
+                    {30, 50, 0}
+                };
+            MatrixCalculations.Display(matrix4, 3, 3);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix4, 3, 3));
+            int[,] matrix5 =
+            {
+                {1,-1,7,-5,3 },
+                {2,5,-3,9,4 },
+                {3,-2,8,1,5 },
+                {4,6,-4,2,6 }
+            };
+            MatrixCalculations.Display(matrix5, 4, 5);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix5, 4, 5));
+
+            int[,] matrix6 =
+            {
+                {1,2,0,5},
+                {2,4,-1,0},
+                {-2,-4,1,0},
+                {1,0,2,1}
+            };
+            MatrixCalculations.Display(matrix6, 4, 4);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix6, 4, 4));
+            int[,] matrix7 =
+            {
+                {0,0,0,0},
+                {0,0,0,0},
+                {0,0,0,0},
+                {0,0,0,0},
+            };
+            MatrixCalculations.Display(matrix7, 4, 4);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix7, 4, 4));
+            int[,] matrix8 =
+            {
+                {1,1,1,1},
+                {1,1,1,1},
+                {1,1,1,1},
+                {0,0,0,0},
+            };
+            MatrixCalculations.Display(matrix8, 4, 4);
+            Console.WriteLine("Rank of the matrix is : "
+                              + MatrixCalculations.RankOfMatrix(matrix8, 4, 4));
+        }
+        static void Main(string[] args)
+		{
+            TestRank();
+            Console.ReadKey();
+        }
+	}
+}
